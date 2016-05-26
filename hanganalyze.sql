26a4757f99c576d9329f3b1a394fa54046822644
Mason Hua <ahdong2007@hotmail.com>
2016-05-26 12:28:54 +0800

    Delete hanganalyze.sql

--- hanganalyze.sql
+++
@@ -1,8 +0,0 @@
-sqlplus -prelim / as sysdba
-oradebug setmypid
-oradebug unlimit
-oradebug hanganalyze 3
-oradebug dump systemstate 266
-REM ......间隔一定时间，如20秒，执行下一次数据采集
-oradebug hanganalyze 3 
-oradebug dump systemstate 266
