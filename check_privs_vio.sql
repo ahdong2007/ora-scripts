efd9d724001a73593173df9c31be243bb1a0cbc5
Mason Hua <ahdong2007@hotmail.com>
2016-06-06 15:06:51 +0800

    add

---
+++ check_privs_vio.sql
@@ -0,0 +1,31 @@
+
+REM
+REM list users granted with DBA/SYSDBA/SYSOPER privs
+REM 06/06/2016 Mason Hua
+REM
+select grantee, granted_role as granted
+  from dba_role_privs
+ where granted_role in ('DBA',
+                        'DATAPUMP_EXP_FULL_DATABASE',
+                        'DATAPUMP_EXP_FULL_DATABASE',
+                        'EXP_FULL_DATABASE',
+                        'EXP_FULL_DATABASE',
+                        'IMP_FULL_DATABASE',
+                        'DELETE_CATALOG_ROLE',
+                        'SELECT_CATALOG_ROLE')
+   and grantee not in ('SYS',
+                       'SYSTEM',
+                       'SYSMAN',
+                       'DATAPUMP_IMP_FULL_DATABASE',
+                       'DBA',
+                       'EXP_FULL_DATABASE',
+                       'IMP_FULL_DATABASE',
+                       'OEM_MONITOR',
+                       'DATAPUMP_EXP_FULL_DATABASE')
+union all
+select username, 'sysdba/oper/asm'
+  from v$pwfile_users
+ where username not in ('SYS',
+                        'SYSTEM')
+   and (sysdba = 'TRUE' or sysoper = 'TRUE' or sysasm = 'TRUE')
+ order by grantee
